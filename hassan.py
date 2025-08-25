39f39b3734f8b8709d03cfac0af65e542b4850da
Hassantaiye <114440007+Hassantaiye@users.noreply.github.com>
2025-08-25 18:54:38 +0100

    Update hassan.py

--- hassan.py
+++ hassan.py
@@ -1,16 +1,16 @@
 file=open("hassan.txt","r")
-# Read the contents of the file
+
 with open("hassan.txt", "r") as file:
     content = file.read()
 
-# Modify the content (example: convert to uppercase)
+
 modified_content = content.upper()
 
-# Write the modified content to a new file
+
 with open("hassan_modified.txt", "w") as new_file:
     new_file.write(modified_content)
 
-# Print the contents of the new file
+
 with open("hassan_modified.txt", "r") as new_file:
     print(new_file.read())
 filename = input("Enter the filename to read: ")
@@ -20,4 +20,5 @@ try:
 except FileNotFoundError:
     print("Error: File does not exist.")
 except IOError:
+
     print("Error: Cannot read the file.")
